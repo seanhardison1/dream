b166c60f93e460d46945bc376819f599c364d375
seanhardison1 <seanhardison@gmail.com>
2020-05-06 13:33:17 -0400

    handles facets

--- R/theme_fade.R
+++ R/theme_fade.R
@@ -10,12 +10,14 @@
 #'    theme_fade()
 
 theme_fade <- function(...) {
-  theme(
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      panel.background = element_blank(),
-      panel.border = element_rect(colour = "black",
-                                  fill = NA, size = 0.2),
-      legend.key = element_blank(),
-      axis.title = element_text(size = 10))
+      ggplot2::theme(
+        strip.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA,
+                                    size=0.2),
+        legend.key = element_blank(),
+        axis.title = element_text(size = 10)
+      )
 }
