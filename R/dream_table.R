f33ea1592a513360367e115da9ba904a22442b14
seanhardison1 <seanhardison@gmail.com>
2020-05-07 08:21:15 -0400

    works with crossed RE intercept (g)lmms fit with lme4

--- R/dream_table.R
+++ R/dream_table.R
@@ -13,29 +13,43 @@
 #' dream_table(model = bovine, caption = "Dream table for Binomial GLMM")
 #' }
 
-dream_table <- function(model, caption){
+dream_table <- function(model, caption = NULL){
   ### Model summaries
-  broom.mixed::tidy(model) %>%
+  fp <- broom.mixed::tidy(model) %>%
     dplyr::mutate(term = ifelse(is.na(std.error),
-                         paste0("$\\sigma_{",group,"}$"), term),
-           effect = ifelse(effect == "fixed", "Fixed", "Random")) %>%
-    dplyr::select(Effect = effect,
-                  Parameter = term,
-                  Estimate = estimate,
-                  `Std. error` = std.error,
-                  `$z$ value` = statistic,
-                  `P-value` = p.value) %>%
-    dplyr::mutate_at(dplyr::vars(Estimate:`P-value`), round, 3) %>%
-    dplyr::mutate_all(tidyr::replace_na, "") %>%
+                                paste0("$\\sigma_{",group,"}$"), term),
+                  effect = ifelse(effect == "fixed", "Fixed", "Random"))
 
-    knitr::kable(., "latex",
-          booktabs = T,
-          escape = F,
-          caption = caption,
-          longtable = T) %>%
+  if (class(model) == "glmmTMB"){
+    sp <- fp %>% dplyr::select(Effect = effect,
+                               Parameter = term,
+                               Estimate = estimate,
+                               `Std. error` = std.error,
+                               `$z$ value` = statistic,
+                               `P-value` = p.value) %>%
+      dplyr::mutate_at(dplyr::vars(Estimate:`P-value`), round, 3) %>%
+      dplyr::mutate_all(tidyr::replace_na, "")
+
+  } else if (class(model) == "glmerMod" | class(model) == "lmerMod") {
+    sp <- fp %>% dplyr::select(Effect = effect,
+                               Parameter = term,
+                               Estimate = estimate,
+                               `Std. error` = std.error,
+                               `$z$ value` = statistic) %>%
+      dplyr::mutate_at(dplyr::vars(Estimate:`$z$ value`), round, 3) %>%
+      dplyr::mutate_all(tidyr::replace_na, "")
+  }
+
+  tble <- sp %>% knitr::kable(., "latex",
+                              booktabs = T,
+                              escape = F,
+                              caption = caption,
+                              longtable = T) %>%
     kableExtra::column_spec(1, bold = T) %>%
     kableExtra::collapse_rows(columns = 1, latex_hline = "major") %>%
     kableExtra::kable_styling(position = "center")
+
+  return(tble)
 }
 
 
