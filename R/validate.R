ba067ef2767bb3924e969a0c5c88e4a6befc3952
seanhardison1 <seanhardison@gmail.com>
2020-08-10 15:31:08 -0400

    removes P adjustment from validate to remain conservative.

--- R/validate.R
+++ R/validate.R
@@ -3,17 +3,21 @@
 #' This function applies DHARMa validation tests to a user-specified list of models. Tests
 #' look for agreement between simulated and empirical residuals; assessing models for dispersion,
 #' zero-inflation, anomalous prevalence of outliers, and deviations from expected distributions.
-#' The philosophy behind this approach is that models being compared using information criteria
+#'
+#' @param mod_set A list of model objects.
+#' @param n Number of DHARMa simulations to generate from each model.
+#' @param p.adjust.method Defaults to \code{none}, but takes any correction method specified with \code{\link{p.adjust}}.
+#'
+#' @details The philosophy behind this approach is that models being compared using information criteria
 #' should meet modeling assumptions regardless of complexity, otherwise model selection
 #' may provide a parsimonious, poorly fitting model (Mazerolle 2019). This method should not
-#' be a replacement for visual inspection of model fit and residuals, but may be useful in an
-#' exploratory context, especially with more complicated hierarchical models.
-#'
-#' @param mod_set A list of model objects
-#' @param n Number of DHARMa simulations to generate
+#' be a replacement for visual inspection of model fit, but may be useful in an
+#' exploratory context, especially with more complicated hierarchical models. To remain conservative,
+#' resultant P values from DHARMa tests are not adjusted for multiple comparison by default, although
+#' P adjustment methods may be specified using \code{p.adjust.method}.
 #'
 #' @return Returns a data.frame containing model formulas for each model and P values for
-#' each test applied. Resultant P values are adjusted following the Benjamini and Hochberg method.
+#' each test applied.
 #'
 #' @export validate
 #'
@@ -25,7 +29,16 @@
 #'
 #' Mazerolle, Marc J. 2019. AICcmodavg: Model Selection and Multimodel Inference Based on (Q)AIC(c). https://cran.r-project.org/package=AICcmodavg.
 #'
-validate <- function(mod_set, n = 1000){
+#' @examples
+#'
+#' library(glmmTMB)
+#' mod_list <- list()
+#' mod_list[[1]] <- glmmTMB(count ~ mined + (1|site), zi=~mined, family=poisson, data=Salamanders)
+#' mod_list[[2]] <- glmmTMB(count ~ mined + cover + (1|site), zi=~mined, family=poisson, data=Salamanders)
+#' mod_list[[3]] <- glmmTMB(count ~ mined + cover + spp + (1|site), zi=~mined, family=poisson, data=Salamanders)
+#' dream::validate(mod_list)
+#'
+validate <- function(mod_set, n = 1000, p.adjust.method = "none"){
   # Do model validation
   df <- do.call(rbind,
                 (lapply(mod_set, loopdy, n = n)))
@@ -37,7 +50,7 @@ validate <- function(mod_set, n = 1000){
     dplyr::select(-response, -predictors) %>%
     dplyr::mutate(id = 1:length(mod_set)) %>%
     tidyr::gather(Var, Value, -id) %>%
-    dplyr::mutate(Value = stats::p.adjust(Value, method = "BH")) %>%
+    dplyr::mutate(Value = stats::p.adjust(Value, method = p.adjust.method)) %>%
     tidyr::spread(Var, Value) %>%
     dplyr::left_join(.,mod_form, by = "id") %>%
     dplyr::select(-id)
