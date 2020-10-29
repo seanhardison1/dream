9cc164df79bdcff426ad551d8f5e56ce2c672012
seanhardison1 <seanhardison@gmail.com>
2020-10-29 11:03:14 -0400

    adds time zone and fixes a broken url error

--- R/query_tac.R
+++ R/query_tac.R
@@ -6,10 +6,11 @@
 #' Larger time steps will result in faster downloads for longer time periods.
 #' @param product Specify data product type. Defaults to "water_temperature".
 #' See a complete listing of available products at \url{https://tidesandcurrents.noaa.gov/api/#products}. Note that all products are
-#' not available at all stations.
+#' not available at all stations. All data are returned in metric units.
 #' @param station Station ID number. A complete listing of stations is available at \url{https://tidesandcurrents.noaa.gov/stations.html}.
 #' @param datum Queries regarding water level require a datum. If none is selected, then datum defaults to "MLLW," or Mean-Lower Low Water. Example datum selections are available to view at \url{https://tidesandcurrents.noaa.gov/datums.html?id=8722607}.
-#' @note All times are GMT.
+#' @param tz Data product time zone. Defaults to GMT ("gmt"). Other options include Local Standard Time ("lst") and Local Standard/Local Daylight Time ("lst_ldt").
+#'
 #'
 #' @export
 #'
@@ -20,12 +21,16 @@
 #'           product = "water_temperature",
 #'           station = 8631044)
 
-query_tac <- function(start, end, product, station, step = "3 days", datum = "MLLW"){
+query_tac <- function(start, end, product,
+                      station, step = "3 days",
+                      datum = "MLLW",
+                      tz = "gmt"){
 
   if (step == "1 day") step <- "1 days"
   step_num <- as.numeric(stringr::str_extract_all(step, "\\d+"))
 
-  if (as.Date(end) - as.Date(start) < step_num){
+  user_end <- end
+  if (as.Date(user_end) - as.Date(start) < step_num){
     stop("Date range for query cannot be less than the length of the selected time step. Choose a smaller time step.")
   } else if (step_num > 30) {
     stop("Time step cannot be greater than 30 days. Choose a smaller time step.")
@@ -33,14 +38,19 @@ query_tac <- function(start, end, product, station, step = "3 days", datum = "ML
 
   query_dates <-
     seq.Date(from = as.Date(start),
-             to = as.Date(end),
+             to = as.Date(user_end),
              by = step)
 
   output <- NULL
   for (i in 1:length(query_dates)){
 
     start <- stringr::str_remove_all(as.character(query_dates[i]), "-")
-    end <- stringr::str_remove_all(as.character(query_dates[i + 1]), "-")
+    if (i != length(query_dates)){
+      end <- stringr::str_remove_all(as.character(query_dates[i + 1]), "-")
+    } else {
+      end <- stringr::str_remove_all(as.character(user_end), "-")
+    }
+
 
     if (product %in% c("water_temperature", "conductivity", "salinity",
                        "currents", "currents_predictions")){
@@ -61,7 +71,8 @@ query_tac <- function(start, end, product, station, step = "3 days", datum = "ML
                     "&application=",server,"&begin_date=", start,
                     "&end_date=",end,
                     "&station=",station,
-                    "&time_zone=GMT&units=english&format=csv&application=UVA")
+                    "&time_zone=",tz,
+                    "&units=metric&format=csv&application=UVA")
 
     fname <- ifelse(server == "NOS.COOPS.TAC.WL", stringr::str_glue(fname, paste0("&datum=",datum)), fname)
 
@@ -75,7 +86,4 @@ query_tac <- function(start, end, product, station, step = "3 days", datum = "ML
   return(dplyr::distinct(output))
 
 }
-
-
-
 
