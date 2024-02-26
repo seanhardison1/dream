35edaf03cc925af28056b4239faa77d256373406
seanhardison1 <seanhardison@gmail.com>
2024-02-26 08:14:35 -0700

    import dplyr

--- R/rst_to_tib.R
+++ R/rst_to_tib.R
@@ -3,18 +3,19 @@
 #'@param r Raster object to be converted.
 #'@param var_name Column name for fill variable. Defaults to `fill_var`.
 #'@param xy_name Column names for x and y variables (in that order). Defaults to `longitude` and `latitude`.
-
+#'
 #' @return A `tibble` with three columns: a fill variable, longitude, and latitude.
 #'
 #' @export
 #'
 #' @examples
-#'
 #' library(raster)
 #' f <- system.file("external/test.grd", package="raster")
 #' r <- raster(f)
 #' tib <- rst_to_tib(r)
 #' tib
+#'
+#' @import dplyr
 
 rst_to_tib <- function(r, var_name = "fill_var",
                       xy_name = c("longitude","latitude")){
